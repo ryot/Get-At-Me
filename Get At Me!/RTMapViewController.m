0f620efaddb80285f9895cf0ea5beee6ebcc696d
Ryo Tulman <ryo.tulman@gmail.com>
2014-05-14 21:49:58 -0700

    Fix create date

--- Get At Me!/RTMapViewController.m
+++ Get At Me!/RTMapViewController.m
@@ -2,7 +2,7 @@
 //  RTMapViewController.m
 //  Get At Me!
 //
-//  Created by Ryo Tulman on 4/8/14.
+//  Created by Ryo Tulman on 5/2/14.
 //  Copyright (c) 2014 Ryo Tulman. All rights reserved.
 //
 
